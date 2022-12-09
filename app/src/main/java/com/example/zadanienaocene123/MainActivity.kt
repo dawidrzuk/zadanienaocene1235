4c5f44210132dcb961fbc6a44d0816c0d8a39981
piasek <piaskowski3456@gmail.com>
2022-12-09 16:08:02 +0100

    koniec

--- app/src/main/java/com/example/zadanienaocene123/MainActivity.kt
+++ app/src/main/java/com/example/zadanienaocene123/MainActivity.kt
@@ -3,42 +3,61 @@ package com.example.zadanienaocene123
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ToggleButton
+
+import android.widget.Switch
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val przycisk1 = findViewById<ToggleButton>(R.id.toggleButton)
-        val przycisk2 = findViewById<ToggleButton>(R.id.toggleButton2)
-        val przycisk3 = findViewById<ToggleButton>(R.id.toggleButton3)
-        val przycisk4 = findViewById<ToggleButton>(R.id.toggleButton4)
-        val przycisk5 = findViewById<ToggleButton>(R.id.toggleButton5)
-        val przycisk6 = findViewById<ToggleButton>(R.id.toggleButton6)
-        val przycisk7 = findViewById<ToggleButton>(R.id.toggleButton7)
-        val przycisk8 = findViewById<ToggleButton>(R.id.toggleButton8)
-        val przycisk9 = findViewById<ToggleButton>(R.id.toggleButton9)
+
         val losuj = findViewById<Button>(R.id.button)
-        val random1 = (0..100).shuffled().last()
-        val random2 = (0..100).shuffled().last()
-        val random3 = (0..100).shuffled().last()
-        val random4 = (0..100).shuffled().last()
-        val random5 = (0..100).shuffled().last()
-        val random6 = (0..100).shuffled().last()
-        val random7 = (0..100).shuffled().last()
-        val random8 = (0..100).shuffled().last()
-        val random9 = (0..100).shuffled().last()
+        val sortuj = findViewById<Button>(R.id.button2)
+        val wynik = findViewById<TextView>(R.id.textView)
+        val lista: List<Switch> = listOf(
+            findViewById(R.id.switch18),
+            findViewById(R.id.switch20),
+            findViewById(R.id.switch21),
+            findViewById(R.id.switch22),
+            findViewById(R.id.switch23),
+            findViewById(R.id.switch24),
+            findViewById(R.id.switch25),
+            findViewById(R.id.switch26),
+            findViewById(R.id.switch27)
+        )
 
         losuj.setOnClickListener {
-            przycisk1.text = random1.toString();
-            przycisk2.text = random2.toString();
-            przycisk3.text = random3.toString();
-            przycisk4.text = random4.toString();
-            przycisk5.text = random5.toString();
-            przycisk6.text = random6.toString();
-            przycisk7.text = random7.toString();
-            przycisk8.text = random8.toString();
-            przycisk9.text = random9.toString();
+            for(i in 0..8)
+            {
+                val liczba = Random.nextInt(0,100)
+                lista[i].text = liczba.toString()
+            }
+            sortuj.setOnClickListener {
+                val lista2: MutableList<Int> = mutableListOf()
+                for (i in 0..8) {
+                    if (lista[i].isChecked)
+                        lista2.add(lista[i].text.toString().toInt())
+                }
+                for (i in 0 until  lista2.size-1)
+                    for(j in 0 until  lista2.size-1)
+                        if(lista2[j] > lista2[j+1])
+                        {
+                            val zamiana = lista2[j]
+                            lista2[j] = lista2[j+1]
+                            lista2[j+1] = zamiana
+                        }
+                if (lista2.isEmpty())
+                {
+                    wynik.text = "Lista jest pusta"
+                }
+                else
+                {
+                    wynik.text = lista2.toString()
+                }
+            }
         }
+
     }
 }
